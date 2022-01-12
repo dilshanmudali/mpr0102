611a166993035f537d87009d7057d294a2ae3bcc
Dilshan <dilshan.mudali@gmail.com>
2022-01-12 18:39:44 -0500

    post route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,14 @@
-
+import PostMessage from '../models/postMessage.js'
 
 export const getPosts = (req,res) => {
-    res.send('Working')
+    try {
+       const postMessages = await PostMessage.find() 
+       res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createPost = (req, res) => {
+    res.send('post created')
 }
