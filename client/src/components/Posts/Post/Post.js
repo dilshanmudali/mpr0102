1c7c3ab8d68144721fc14dd5a171648a88043fa3
Dilshan <dilshan.mudali@gmail.com>
2022-01-31 23:32:28 -0500

    post fetch

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import useStyles from './styles'
+import {Card, CardAction, CardContent, CardMedia, Button, Typography} from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAl'
+import DeleteIcon from '@material-ui/icons/DeleteIcon'
+import MoreHorizIcon from '@material-ui/icons/MoreHorizIcon'
 
-const Post = () => {
+const Post = ({post}) => {
     const classes = useStyles()
     return (
-        <div>
-            <h1>Post</h1>
-        </div>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2}>
+                <Button></Button>
+            </div>
+        </Card>
     )
 }
 
