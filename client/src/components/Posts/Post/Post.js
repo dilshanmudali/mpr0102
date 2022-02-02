1af82d9e33363cb60202981e97140f51c3b03808
Dilshan <dilshan.mudali@gmail.com>
2022-02-02 18:28:54 -0500

    post jsx update

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import useStyles from './styles'
 import {Card, CardAction, CardContent, CardMedia, Button, Typography} from '@material-ui/core'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAl'
-import DeleteIcon from '@material-ui/icons/DeleteIcon'
-import MoreHorizIcon from '@material-ui/icons/MoreHorizIcon'
+// import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+// import DeleteIcon from '@material-ui/icons/Delete'
+// import MoreHorizIcon from '@material-ui/icons/MoreHorizIcon';
+import moment from 'moment'
 
 const Post = ({post}) => {
     const classes = useStyles()
@@ -15,8 +16,16 @@ const Post = ({post}) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button></Button>
+                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                    {/* <MoreHorizIcon fontSize="default"/> */}
+                </Button>
             </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map(tag => `#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
         </Card>
     )
 }
